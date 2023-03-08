e5ec5168c5ac736ae620d803d9dfbba723545e55
Melyn Atieno Were <melynatieno@gmail.com>
2023-03-08 17:45:07 +0300

    add styles folder

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import "../Styles/Home.css"
 
 function Home() {
   return (
     <div>
         <h1>WELCOME ! </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Impedit corrupti neque, a sapiente, expedita magni culpa est, 
+          voluptas at commodi sequi inventore possimus similique hic aut dolorum odio quia nam?</p>
     </div>
   )
 }
