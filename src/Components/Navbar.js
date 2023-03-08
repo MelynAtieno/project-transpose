cc7e78d5387b6683debf429a99a6fdcb9ca57c07
Melyn Atieno Were <melynatieno@gmail.com>
2023-03-08 17:21:26 +0300

    style navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import "../Styles/Navbar.css"
 
 function Navbar() {
   return (
     <div className='nav'>
         <ul>
-            <li><a href='#home'>HOME</a></li>
-            <li><a href='#about'>ABOUT</a></li>
-            <li><a href='#contact'>CONTACT</a></li>
+            <li className='nav-item'><a href='home' className='nav-link'>HOME</a></li>
+            <li className='nav-item'><a href='about' className='nav-link'>ABOUT</a></li>
+            <li className='nav-item'><a href='contact' className='nav-link'>CONTACT</a></li>
         </ul>
     </div>
   )
