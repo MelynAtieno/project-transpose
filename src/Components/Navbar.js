5d8dd5d5ea376448ce95e8aebfe8915d5857e155
Melyn Atieno Were <melynatieno@gmail.com>
2023-03-08 14:22:38 +0300

    add navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <ul>
+            <li><a href='#home'>HOME</a></li>
+            <li><a href='#about'>ABOUT</a></li>
+            <li><a href='#contact'>CONTACT</a></li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
