ed2c9580683bd2bea4a238731d6efdaaea983cfe
Melyn Atieno Were <melynatieno@gmail.com>
2023-03-08 14:28:56 +0300

    add about page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
+import About from './Components/About';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
+      <About />
     </div>
   );
 }
---
+++ src/Components/About.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+        <h1>ABOUT US</h1>
+        <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Ratione ipsum dolor consectetur corporis soluta ullam architecto commodi 
+        veritatis eum quas error voluptatem officiis laborum voluptatibus, reiciendis maiores qui unde ut.
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        Nam atque, sapiente exercitationem tenetur ullam quasi quis ipsa voluptate,
+        autem culpa voluptatibus, ipsum corporis assumenda minus animi quaerat eveniet asperiores. Saepe.
+  
+        </p>
+        <button>LEARN MORE</button>
+    </div>
+  )
+}
+
+export default About
